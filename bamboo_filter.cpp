13102a5fd5e9d78e294a77dfdf8a00f0ce59c833
Petra Buršić <bursic.petra1@gmail.com>
2024-05-17 22:40:25 +0200

    Bamboo filter implementation

--- bamboo_filter.cpp
+++ bamboo_filter.cpp
@@ -1,48 +1,120 @@
 #include "bamboo_filter.hpp"
 #include <iostream>
-#include <cmath>
+#include <vector>
 
 BambooFilter::BambooFilter(size_t initial_capacity, size_t seg_size)
-    : segment_size(seg_size)
+    : segment_size(seg_size), elements_count(0),
+      expansion_threshold(initial_capacity), compression_threshold(initial_capacity / 2)
 {
     segments.reserve(initial_capacity / segment_size);
     for (size_t i = 0; i < initial_capacity / segment_size; ++i)
     {
-        // segments.push_back({CuckooFilter(seg_size, 4, 10)});  // Assumed bucket size and fingerprint size
+        segments.emplace_back();
     }
 }
 
-bool BambooFilter::insert(const int &element)
+BambooFilter::~BambooFilter()
 {
-    size_t hash_val = hash(element);
-    size_t segment_index = get_segment_index(hash_val);
-
-    // return segments[segment_index].insert(element);
+    for (auto &segment : segments)
+    {
+        segment.clear();
+    }
+    segments.clear();
 }
 
-bool BambooFilter::lookup(const int &element) const
+bool BambooFilter::insert(const uint32_t &element)
 {
     size_t hash_val = hash(element);
     size_t segment_index = get_segment_index(hash_val);
 
-    // return segments[segment_index].lookup(element);
+    if (segments[segment_index].insert(element))
+    {
+        elements_count++;
+        if (elements_count > expansion_threshold)
+        {
+            expand();
+        }
+        return true;
+    }
+
+    expand();
+    return insert(element);
 }
 
-bool BambooFilter::remove(const int &element)
+bool BambooFilter::lookup(const uint32_t &element) const
 {
     size_t hash_val = hash(element);
     size_t segment_index = get_segment_index(hash_val);
 
-    // return segments[segment_index].remove(element);
+    return segments[segment_index].lookup(element);
+}
+
+bool BambooFilter::remove(const uint32_t &element)
+{
+    size_t hash_val = hash(element);
+    size_t segment_index = get_segment_index(hash_val);
+
+    if (segments[segment_index].remove(element))
+    {
+        elements_count--;
+        if (elements_count < compression_threshold)
+        {
+            compress();
+        }
+        return true;
+    }
+    return false;
 }
 
 void BambooFilter::expand()
 {
+    cout << "Expanding..." << "\n";
+    segments.emplace_back();
+
+    elements_count = 0;
+    std::vector<uint32_t> elements_to_rehash;
+    for (auto &segment : segments)
+    {
+        segment.collect_elements(elements_to_rehash);
+        segment.clear();
+    }
+    for (auto &element : elements_to_rehash)
+    {
+        insert(element);
+    }
+
+    expansion_threshold *= 2;
+    compression_threshold = expansion_threshold / 2;
 }
 
-size_t BambooFilter::hash(const int &element) const
+void BambooFilter::compress()
 {
-    return std::hash<int>()(element);
+    cout << "Compressing..." << "\n";
+
+    if (!segments.empty())
+    {
+        segments.pop_back();
+    }
+
+    elements_count = 0;
+    std::vector<uint32_t> elements_to_rehash;
+    for (auto &segment : segments)
+    {
+        segment.collect_elements(elements_to_rehash);
+        segment.clear();
+    }
+    for (auto &element : elements_to_rehash)
+    {
+        insert(element);
+    }
+
+    expansion_threshold /= 2;
+    compression_threshold = expansion_threshold / 2;
+}
+
+size_t BambooFilter::hash(const uint32_t &element) const
+{
+    return std::hash<uint32_t>()(element);
 }
 
 size_t BambooFilter::get_segment_index(size_t hash_value) const
--- bamboo_filter.hpp
+++ bamboo_filter.hpp
@@ -8,19 +8,23 @@ class BambooFilter
 {
 public:
     BambooFilter(size_t initial_capacity, size_t segment_size);
-    ~BambooFilter();
+    ~BambooFilter(); // Deklaracija destruktora
 
-    bool insert(const int &element);
-    bool lookup(const int &element) const;
-    bool remove(const int &element);
+    bool insert(const uint32_t &element);
+    bool lookup(const uint32_t &element) const;
+    bool remove(const uint32_t &element);
     void expand();
+    void compress();
 
 private:
     std::vector<Segment> segments;
     size_t segment_size;
+    size_t elements_count;
+    size_t expansion_threshold;
+    size_t compression_threshold;
 
-    size_t hash(const int &element) const;
+    size_t hash(const uint32_t &element) const;
     size_t get_segment_index(size_t hash_value) const;
 };
 
-#endif
+#endif // BAMBOO_FILTER_H
