1eff1e02493cea4783110c6f17610ad5a5198b22
Petra Buršić <bursic.petra1@gmail.com>
2024-05-17 20:52:19 +0200

    Revert "Added methods clear and collect_elements to segment"
    This reverts commit d96561adf92ab0efa803b221da5a6d090a69c063.

--- segment.hpp
+++ segment.hpp
@@ -3,24 +3,19 @@
 
 #include <cstdint>
 
-using namespace std;
-
 class Segment
 {
 private:
     // two dimensional array with kNumOfBuckets rows and kBucketSize columns; implemented as one dimensional to achieve memory locality
-    uint32_t *table_;
+    uint32_t* table_;
     // pointer to this segment's overflow segment
-    Segment *overflow_;
-
+    Segment* overflow_;
 public:
     Segment();
     ~Segment();
     bool insert(uint32_t element);
-    bool lookup(uint32_t element) const;
+    bool lookup(uint32_t element);
     bool remove(uint32_t element);
-    void collect_elements(vector<uint32_t> &elements) const;
-    void clear();
 };
 
 #endif
