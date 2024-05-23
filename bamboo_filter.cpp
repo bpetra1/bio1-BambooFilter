81e3a4026bb0ec1ee7354dcf45fefeb8a8fdf7c6
Petra Buršić <bursic.petra1@gmail.com>
2024-05-24 00:07:23 +0200

    Updated, so the bamboo_filter can work with strings

--- bamboo_filter.cpp
+++ bamboo_filter.cpp
@@ -1,6 +1,9 @@
 #include "bamboo_filter.hpp"
+#include "constants.hpp"
 #include <iostream>
 #include <vector>
+#include <string>
+#include <functional>
 
 BambooFilter::BambooFilter(size_t initial_capacity, size_t seg_size)
     : segment_size(seg_size), elements_count(0),
@@ -22,9 +25,14 @@ BambooFilter::~BambooFilter()
     segments.clear();
 }
 
-bool BambooFilter::insert(const uint32_t &element)
+bool BambooFilter::insert(const std::string &element)
 {
     size_t hash_val = hash(element);
+    return insert_hash(hash_val);
+}
+
+bool BambooFilter::insert_hash(const size_t &hash_val)
+{
     size_t segment_index = get_segment_index(hash_val);
 
     if (segments[segment_index].insert(hash_val))
@@ -38,20 +46,29 @@ bool BambooFilter::insert(const uint32_t &element)
     }
 
     expand();
-    return insert(element);
+    return insert_hash(hash_val);
 }
 
-bool BambooFilter::lookup(const uint32_t &element) const
+bool BambooFilter::lookup(const std::string &element) const
 {
     size_t hash_val = hash(element);
-    size_t segment_index = get_segment_index(hash_val);
+    return lookup_hash(hash_val);
+}
 
+bool BambooFilter::lookup_hash(const size_t &hash_val) const
+{
+    size_t segment_index = get_segment_index(hash_val);
     return segments[segment_index].lookup(hash_val);
 }
 
-bool BambooFilter::remove(const uint32_t &element)
+bool BambooFilter::remove(const std::string &element)
 {
     size_t hash_val = hash(element);
+    return remove_hash(hash_val);
+}
+
+bool BambooFilter::remove_hash(const size_t &hash_val)
+{
     size_t segment_index = get_segment_index(hash_val);
 
     if (segments[segment_index].remove(hash_val))
@@ -68,19 +85,19 @@ bool BambooFilter::remove(const uint32_t &element)
 
 void BambooFilter::expand()
 {
-    cout << "Expanding..." << "\n";
+    std::cout << "Expanding..." << "\n";
     segments.emplace_back();
 
     elements_count = 0;
     std::vector<size_t> elements_to_rehash;
-    for (int i = 0; i < segments.size(); i++)
+    for (size_t i = 0; i < segments.size(); i++)
     {
         segments[i].collect_elements(elements_to_rehash, i);
         segments[i].clear();
     }
     for (auto &element : elements_to_rehash)
     {
-        insert(element);
+        insert_hash(element);
     }
 
     expansion_threshold *= 2;
@@ -89,7 +106,7 @@ void BambooFilter::expand()
 
 void BambooFilter::compress()
 {
-    cout << "Compressing..." << "\n";
+    std::cout << "Compressing..." << "\n";
 
     if (!segments.empty())
     {
@@ -98,26 +115,28 @@ void BambooFilter::compress()
 
     elements_count = 0;
     std::vector<size_t> elements_to_rehash;
-    for (int i = 0; i < segments.size(); i++)
+    for (size_t i = 0; i < segments.size(); i++)
     {
         segments[i].collect_elements(elements_to_rehash, i);
         segments[i].clear();
     }
     for (auto &element : elements_to_rehash)
     {
-        insert(element);
+        insert_hash(element);
     }
 
     expansion_threshold /= 2;
     compression_threshold = expansion_threshold / 2;
 }
 
-size_t BambooFilter::hash(const uint32_t &element) const
+size_t BambooFilter::hash(const std::string &element) const
 {
-    return std::hash<uint32_t>()(element);
+    return std::hash<std::string>()(element);
 }
 
 size_t BambooFilter::get_segment_index(size_t hash_value) const
 {
-    return hash_value % segments.size();
+    // Extract the segment index from the hash value based on the bit lengths
+    size_t segment_index = (hash_value >> kBucketIndexBitLength) & ((1 << kInitialSegmentIndexBitLength) - 1);
+    return segment_index;
 }
--- bamboo_filter.hpp
+++ bamboo_filter.hpp
@@ -3,18 +3,17 @@
 
 #include "segment.hpp"
 #include <vector>
+#include <string>
 
 class BambooFilter
 {
 public:
     BambooFilter(size_t initial_capacity, size_t segment_size);
-    ~BambooFilter(); // Deklaracija destruktora
+    ~BambooFilter(); // Declaration of destructor
 
-    bool insert(const uint32_t &element);
-    bool lookup(const uint32_t &element) const;
-    bool remove(const uint32_t &element);
-    void expand();
-    void compress();
+    bool insert(const std::string &element);
+    bool lookup(const std::string &element) const;
+    bool remove(const std::string &element);
 
 private:
     std::vector<Segment> segments;
@@ -23,8 +22,14 @@ private:
     size_t expansion_threshold;
     size_t compression_threshold;
 
-    size_t hash(const uint32_t &element) const;
+    size_t hash(const std::string &element) const;
     size_t get_segment_index(size_t hash_value) const;
+
+    bool insert_hash(const size_t &hash_val);
+    bool lookup_hash(const size_t &hash_val) const;
+    bool remove_hash(const size_t &hash_val);
+    void expand();
+    void compress();
 };
 
 #endif // BAMBOO_FILTER_H
