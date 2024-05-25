51c2760f8ac4bed90b1547c4b441a2e545f1a88d
Petra Buršić <bursic.petra1@gmail.com>
2024-05-25 22:49:34 +0200

    Revert "printing inserted elements"
    This reverts commit 825c479a1a4f74a552a1188287f608f2b1e41c3d.

--- bamboo_filter.hpp
+++ bamboo_filter.hpp
@@ -8,7 +8,7 @@
 class BambooFilter
 {
 public:
-    BambooFilter(size_t initial_capacity, size_t seg_size);
+    BambooFilter(size_t initial_capacity, size_t segment_size);
     ~BambooFilter();
 
     bool insert(const std::string &element);
@@ -16,23 +16,22 @@ public:
     bool remove(const std::string &element);
 
 private:
-    size_t hash(const std::string &element) const;
-    size_t get_segment_index(size_t hash_value) const;
     bool insert_hash(const size_t &hash_val);
     bool lookup_hash(const size_t &hash_val) const;
     bool remove_hash(const size_t &hash_val);
 
     void expand();
     void compress();
-    void update_thresholds();
 
+    size_t hash(const std::string &element) const;
+    size_t get_segment_index(size_t hash_value) const;
+
+    std::vector<Segment> segments;
     size_t segment_size;
     size_t elements_count;
     size_t current_segment_index;
     size_t expansion_threshold;
     size_t compression_threshold;
-
-    std::vector<Segment> segments;
 };
 
 #endif // BAMBOO_FILTER_HPP
